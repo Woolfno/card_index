998d9e1fe268ab6cab0ffa7c38ef4f1815b16926
Woolf <woolf.no1@gmail.com>
2024-11-06 11:25:06 +0500

    Feature: aerich migrations tool

--- db/db.py
+++ db/db.py
@@ -2,15 +2,18 @@ from tortoise import Tortoise, connections
 
 from settings import settings
 
-# positions=[Position(id=0, title="Accountant"), Position(id=1, title="CEO")]
-# boss = Employee(id=uuid.uuid4(), first_name="Ivan",   middle_name="Petrovich", last_name="Smirnov", position=positions[1], start_date=datetime.date(2024, 1, 1), salary=Decimal(2500))
-# employes = [
-#     boss,
-#     Employee(id=uuid.uuid4(), boss=boss, first_name="Tomara", middle_name="Ivanovna",  last_name="Shpak",   position=positions[0], start_date=datetime.date(2024, 5, 2), salary=Decimal(500)),
-# ]
+TORTOISE_ORM = {
+    "connections": {"default": settings.DATABASE_URL},
+    "apps": {
+        "models": {
+            "models": ["models.models", "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
 
 async def init_tortoise()->None:
-    await Tortoise.init(db_url=settings.DATABASE_URL, modules={"models": ["models.models"]})
+    await Tortoise.init(TORTOISE_ORM)
     await Tortoise.generate_schemas()
 
 async def shutdown_tortoise()->None:
---
+++ migrations/models/1_20241106111907_None.py
@@ -0,0 +1,30 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE TABLE IF NOT EXISTS "position" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "title" VARCHAR(100) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "employee" (
+    "uuid" UUID NOT NULL  PRIMARY KEY,
+    "first_name" VARCHAR(120) NOT NULL,
+    "middle_name" VARCHAR(120) NOT NULL,
+    "last_name" VARCHAR(120) NOT NULL,
+    "start_date" DATE NOT NULL,
+    "salary" DECIMAL(2,2) NOT NULL,
+    "boss_id" UUID REFERENCES "employee" ("uuid") ON DELETE SET NULL,
+    "position_id" INT NOT NULL REFERENCES "position" ("id") ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS "aerich" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "version" VARCHAR(255) NOT NULL,
+    "app" VARCHAR(100) NOT NULL,
+    "content" JSONB NOT NULL
+);"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        """
