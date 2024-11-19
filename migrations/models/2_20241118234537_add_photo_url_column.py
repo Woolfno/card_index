3824d26d4df0c1f9de5ea5fade018855bca3606d
Woolf <woolf.no1@gmail.com>
2024-11-19 21:15:46 +0500

    Fix: two migration merge in one

--- migrations/models/2_20241118234537_add_photo_url_column.py
+++ migrations/models/2_20241118234537_add_photo_url_column.py
@@ -3,11 +3,11 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        ALTER TABLE "employee" ADD "photo_filename" VARCHAR(255);
+        ALTER TABLE "employee" ADD "photo_url" VARCHAR(255);
         CREATE UNIQUE INDEX "uid_user_usernam_9987ab" ON "user" ("username");"""
 
 
 async def downgrade(db: BaseDBAsyncClient) -> str:
     return """
         DROP INDEX "idx_user_usernam_9987ab";
-        ALTER TABLE "employee" DROP COLUMN "photo_filename";"""
+        ALTER TABLE "employee" DROP COLUMN "photo_url";"""
