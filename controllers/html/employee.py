cd78779e6e5b3568d32600f40d7b496572f0f38e
Woolf <woolf.no1@gmail.com>
2024-11-20 20:49:40 +0500

    Fix: add employee service layer

--- controllers/html/employee.py
+++ controllers/html/employee.py
@@ -1,12 +1,8 @@
-from pathlib import Path
 from typing import Annotated
 from uuid import UUID
 
-import aiofiles
-from datetime import datetime
 from litestar import get, post
 from litestar.controller import Controller
-from litestar.datastructures import UploadFile
 from litestar.enums import RequestEncodingType
 from litestar.exceptions import InternalServerException, NotFoundException
 from litestar.params import Body
@@ -15,8 +11,8 @@ from tortoise.exceptions import OperationalError
 from tortoise.transactions import in_transaction
 
 from models.models import Employee, Position
-from schemas.employee import EmployeeWithPhoto
-from settings import settings
+from schemas.employee import EmployeeIn, EmployeeWithPhoto
+from services.employee import EmployeeService
 
 
 class EmployeeController(Controller):
@@ -30,31 +26,11 @@ class EmployeeController(Controller):
         return Template(template_name="employee/create.html", 
                         context={"positions": positions, "bosses":employees})
     
-    async def _save_file(self, src:UploadFile, dst:str):
-        async with aiofiles.open(dst, "wb") as out:
-            while content:=await src.read(1024):
-                await out.write(content)      
-    
-    # path to save on db and view in html page
-    def _get_fileurl(self, filename:str)->str:
-        return Path(settings.MEDIA_URL) / filename
-    
-    # path to save on disk
-    def _get_filepath(self, filename:str)->str:
-        return Path(settings.MEDIA_ROOT) / filename
-            
-    def _generate_filename(self, filename:str)->str:
-        name, ext = filename.split('.')
-        return f"{datetime.now().strftime("%d%m%Y%H%M%S")}_{name}.{ext}"
-
     @post("/create")
     async def create(self,
-                     data:Annotated[EmployeeWithPhoto, Body(media_type=RequestEncodingType.MULTI_PART)])->Redirect:
-        filename = self._generate_filename(data.photo_file.filename)
-        e = await Employee.create(**data.model_dump(exclude_none=True, exclude_unset=True),
-                                  photo_url=self._get_fileurl(filename))
-        filepath = self._get_filepath(filename)
-        await self._save_file(data.photo_file, filepath)
+                     data:Annotated[EmployeeWithPhoto, Body(media_type=RequestEncodingType.MULTI_PART)])->Redirect:        
+        employe_in = EmployeeIn(**data.model_dump(exclude="photo_file"))        
+        e = await EmployeeService.create(employe_in, data.photo_file)
         return Redirect(path=f"/employee/{e.id}") 
     
     @get("/edit/{id:uuid}")
@@ -71,30 +47,16 @@ class EmployeeController(Controller):
     async def update(self, 
                      data:Annotated[EmployeeWithPhoto, Body(media_type=RequestEncodingType.MULTI_PART)], 
                      id:UUID)->Redirect:        
-        try:
-            async with in_transaction() as connection:
-                e = await Employee.get_or_none(id=id, using_db=connection)             
-                if e is None:
-                    raise NotFoundException
-                if data.photo_file is not None:
-                    filename = self._generate_filename(data.photo_file.filename)
-                    filepath = self._get_filepath(filename)
-                    await self._save_file(data.photo_file, filepath)
-                    await Employee.filter(id=id).using_db(connection).update(
-                        **data.model_dump(exclude_unset=True, exclude_none=True, exclude="photo_file"), 
-                        photo_url=self._get_fileurl(filename),
-                        )
-                else:
-                    await Employee.filter(id=id).using_db(connection).update(
-                        **data.model_dump(exclude_unset=True, exclude_none=True),
-                        )                    
-        except OperationalError:
-            raise InternalServerException
-        return Redirect(f"/employee/{id}")
+        employee_in = EmployeeIn(**data.model_dump(exclude="photo_file"))
+        photo_file = data.photo_file
+        e = await EmployeeService.update(id, employee_in, photo_file)
+        if e is None:
+            raise NotFoundException
+        return Redirect(f"/employee/{e.id}")
     
     @get("/{id:uuid}")
     async def get_employee(self, id:UUID)->Template:
-        e = await Employee.get_or_none(id=id).prefetch_related("position", "boss")
+        e = await EmployeeService.get_by_id(id)
         if e is None:
             raise NotFoundException
         return Template(template_name="employee/card.html", 
@@ -102,10 +64,7 @@ class EmployeeController(Controller):
     
     @get("/delete/{id:uuid}")
     async def delete_employee(self, id:UUID)->Redirect:
-        async with in_transaction() as connection:
-            e = await Employee.get_or_none(id=id, using_db=connection)
-            if e is None:
-                return Redirect("/tree")
-            await e.delete(using_db=connection)
+        if await EmployeeService.remove(id):
             return Redirect("/tree")
+        return Redirect('/tree')
     
